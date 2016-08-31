387567ba924d2a2fc44c3f2d893e47f26f31f397
Mathew Piccinato <mpiccinato@gmail.com>
2016-08-31 08:41:05 -0400

    Installs Oh My ZSH

---
+++ zsh/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Oh My ZSH
+#
+# Installs Oh My ZSH
+
+echo "  Installing Oh My ZSH for you."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+exit 0
