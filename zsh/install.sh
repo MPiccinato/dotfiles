a6f31b1e8d229d160486851ad2776c334d48225f
MPiccinato <mpiccinato@gmail.com>
2016-09-02 10:02:45 -0400

    Copy crunch custom theme on installation

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,5 +6,6 @@
 
 echo "  Installing Oh My ZSH for you."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp zsh/crunch-custom.zsh-theme ~/.oh-my-zsh/themes/crunch-custom.zsh-theme
 
 exit 0
